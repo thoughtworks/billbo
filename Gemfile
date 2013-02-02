ecb6c58e29cd184fd3d4cede782d13dd09b3616f
Junior Ales <edilson.ales.jr@gmail.com>
2013-02-02 19:58:23 -0200

    Setup redis

--- Gemfile
+++ Gemfile
@@ -1,8 +1,9 @@
 source 'https://rubygems.org'
 
 gem 'sinatra', ' 1.3.4'
+gem 'redis', '3.0.2'
 
 group :test, :development do
-	gem 'shotgun'
+	gem 'shotgun', '0.9'
 end
 
--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require "bundler/setup"
 require "sinatra"
 
+configure do
+	require "redis"
+	redisUri = ENV["REDISTOGO_URL"] || 'redis://localhost:6379'
+	uri = URI.parse(redisUri)
+	REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
+
 get "/" do
-	"Hello world!"
+	"Hello World!"
 end
