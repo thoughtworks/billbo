92ba9343cd185727b32afbdedfc820ac8fe30ddf
Junior Ales <edilson.ales.jr@gmail.com>
2013-02-02 23:16:14 -0200

    Redis config updated to use 1.9 ruby syntax

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ configure do
 	require "redis"
 	redisUri = ENV["REDISCLOUD_URL"] || 'redis://localhost:6379'
 	uri = URI.parse(redisUri)
-	REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+	REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 end
 
 get "/" do
