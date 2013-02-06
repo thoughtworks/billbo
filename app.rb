80ebd3bf20180589563b4054c51b2c271ec9f6d0
Junior Ales <edilson.ales.jr@gmail.com>
2013-02-06 01:08:32 -0200

    Refactoring quotes

--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
-require "bundler/setup"
-require "sinatra"
+require 'bundler/setup'
+require './model/bill'
+Bundler.require
 
 configure do
-	require "redis"
-	redisUri = ENV["REDISCLOUD_URL"] || 'redis://localhost:6379'
+	require 'redis'
+	redisUri = ENV['REDISCLOUD_URL'] || 'redis://localhost:6379'
 	uri = URI.parse(redisUri)
 	REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 end
 
-get "/" do
-	REDIS.set("welcome", "Hello World")
+get '/' do
 	erb :index
 end
