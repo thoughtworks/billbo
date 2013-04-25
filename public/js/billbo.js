823eaa45928618906bb39f3e9d6ade1710a395a4
Junior Ales <edilson.ales.jr@gmail.com>
2013-04-25 16:00:02 -0300

    Showing and hiding bill details properly

--- public/js/billbo.js
+++ public/js/billbo.js
@@ -1,12 +1,21 @@
+var hideAllBillDetails = function() {
+  $(".bill-details").removeClass("active-box");
+  $(".bill-container").removeClass("after-active-box");
+}
+
 $(document).ready(function() {
   $(".bill-box").on("click", function(event) {
     event.preventDefault();
-    $(".bill-details").removeClass("active-box");
-    $(".bill-container").removeClass("after-active-box");
+    hideAllBillDetails();
 
     var container = $(this).closest(".bill-container");
     $(".thumbnails").prepend(container);
     container.find(".bill-details").addClass("active-box");
     container.next().addClass("after-active-box");
+  });
+
+  $(".close-bill-details").on("click", function(event) {
+    event.preventDefault();
+    hideAllBillDetails();
   });
 });
