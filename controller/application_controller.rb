177b9d7d2e16ea57a43af9aac156843fc9c7003b
vivitc <vivitc@gmail.com>
2013-07-24 15:06:53 -0300

    [+Gonzalo] Fixing server side validation for bill.

--- controller/application_controller.rb
+++ controller/application_controller.rb
@@ -14,7 +14,7 @@ end
 
 post '/bill/create' do
   bill = Bill.create(params)
-  if bill
+  if bill.save
     redirect '/bill/new', :success => i18n.bill_creation_success
   else
     redirect '/bill/new', :error => i18n.bill_creation_fail
