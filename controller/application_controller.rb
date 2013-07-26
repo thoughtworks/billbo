fc5300eb9e140af69bea1f3392cf64597b3fc551
Junior Ales <edilson.ales.jr@gmail.com>
2013-07-26 01:58:54 -0300

    Adding error messages according with laguage set and better form validations

--- controller/application_controller.rb
+++ controller/application_controller.rb
@@ -1,6 +1,15 @@
 before do
-  session[:locale] = params[:locale] if params[:locale]
-  session[:locale] = 'pt' unless session[:locale]
+  session[:locale] ||= 'pt'
+  session[:i18n_hash] ||= localeLabels(session[:locale])
+
+  if params[:locale]
+    session[:locale] = params[:locale]
+    session[:i18n_hash] = localeLabels(session[:locale])
+  end
+end
+
+def localeLabels localeCode
+  YAML::load(File.read("i18n/#{localeCode}.yml")).to_json
 end
 
 get '/' do
@@ -15,7 +24,7 @@ end
 post '/bill/create' do
   bill = Bill.create(params)
   if bill.save
-    redirect '/bill/new', :success => i18n.bill_creation_success
+    redirect '/bill/new', :success => i18n.bill_creation_ok
   else
     redirect '/bill/new', :error => i18n.bill_creation_fail
   end
