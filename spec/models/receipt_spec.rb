ae51f83847b0d8df48b0b275519110cccbcffae5
Rafael Garcia <rafbgarcia@gmail.com>
2013-08-14 14:16:29 -0300

    Added receipt tests

---
+++ spec/models/receipt_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Receipt do
+
+  context :relations do
+    it { should belong_to(:bill) }
+  end
+
+  context :validations do
+    it { should validate_presence_of(:contributor_email) }
+  end
+
+end
