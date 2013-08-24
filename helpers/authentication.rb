daf62d11f70fa3368ec47b352ec502de5fc8bdc9
Brian Thomas Storti <btstorti@gmail.com>
2013-08-23 23:01:52 -0300

    include sinatra helpers

--- helpers/authentication.rb
+++ helpers/authentication.rb
@@ -1,9 +1,11 @@
 # encoding: UTF-8
 
-def logged_in
-  session[:email].present?
-end
+helpers do
+  def logged_in
+    session[:email].present?
+  end
 
-def logged_as_admin?
-  logged_in && Admin.where(email: session[:email]).any?
+  def logged_as_admin?
+    logged_in && Admin.where(email: session[:email]).any?
+  end
 end
--- helpers/locale.rb
+++ helpers/locale.rb
@@ -1,25 +1,27 @@
 # encoding: UTF-8
 
-def setup_locale
-  I18n.locale = session[:locale] || :pt
+helpers do
+  def setup_locale
+    I18n.locale = session[:locale] || :pt
 
-  if params[:locale]
-    session[:locale] = params[:locale]
-    I18n.locale = params[:locale]
+    if params[:locale]
+      session[:locale] = params[:locale]
+      I18n.locale = params[:locale]
+    end
+
+    session[:i18n_hash] = locale_labels(session[:locale])
   end
 
-  session[:i18n_hash] = locale_labels(session[:locale])
-end
+  def locale_labels(locale_code)
+    locale_file = "#{settings.root}/i18n/#{locale_code}.yml"
 
-def locale_labels(locale_code)
-  locale_file = "#{settings.root}/i18n/#{locale_code}.yml"
+    unless File.exist?(locale_file)
+      default_locale_file = "#{settings.root}/i18n/pt.yml"
+      locale_file = default_locale_file
+      R18n::I18n.default = "pt"
+      session[:locale] = "pt"
+    end
 
-  unless File.exist?(locale_file)
-    default_locale_file = "#{settings.root}/i18n/pt.yml"
-    locale_file = default_locale_file
-    R18n::I18n.default = "pt"
-    session[:locale] = "pt"
+    YAML::load(File.read(locale_file)).to_json
   end
-
-  YAML::load(File.read(locale_file)).to_json
 end
