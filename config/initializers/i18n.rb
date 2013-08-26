9cc4dfabe09a37896a06ba409c101dd632f15e68
Brian Thomas Storti <btstorti@gmail.com>
2013-08-26 10:23:49 -0300

    use locale_path variable

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -3,7 +3,7 @@
 set :available_locales, [:en, :pt]
 set :locales_path, "#{settings.root}/config/locales"
 
-Dir.glob("#{settings.root}/config/locales/**/*.yml").each do |file|
+Dir.glob("#{settings.locales_path}/**/*.yml").each do |file|
   I18n.load_path << file
 end
 
