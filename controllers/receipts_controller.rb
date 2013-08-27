f474b1c212a72bf537fe6174853dd0fa924c98e9
Brian Thomas Storti <btstorti@gmail.com>
2013-08-27 12:19:57 -0300

    send a single e-mail to all admins

--- controllers/receipts_controller.rb
+++ controllers/receipts_controller.rb
@@ -21,14 +21,13 @@ private
 
 # FIXME this should not be here
 def send_email(payment, bill)
-  Admin.all.each do |admin|
-    Pony.mail :to => admin.email,
-          :from => payment.contributor_email,
-          :subject => I18n.t(:upload_receipt_subject),
-          :html_body => erb(:"/mails/payment_confirmation",
-                            :locals => {:receipt => payment, :bill => bill },
-                            :layout => false),
-          :via => :smtp
+  recipients = Admin.pluck(:email).join(",")
+  Pony.mail(:to => recipients,
+            :from => payment.contributor_email,
+            :subject => I18n.t(:upload_receipt_subject),
+            :html_body => erb(:"/mails/payment_confirmation",
+                              :locals => {:receipt => payment, :bill => bill },
+                              :layout => false),
+                              :via => :smtp)
 
-  end
 end
