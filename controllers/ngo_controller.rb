9efb29adcbf889e8b38379fc80dbd7c6fe00f9d1
Isabella Albuquerque <ialbuqu@thoughtworks.com>
2013-10-24 17:26:54 -0300

    Adding share on facebook and twitter functionality

--- controllers/ngo_controller.rb
+++ controllers/ngo_controller.rb
@@ -4,3 +4,8 @@ get '/ngo/:id' do
   @ngo = Ngo.find(params[:id])
   erb :"/ngo/index", locals: { errors: [] }
 end
+
+get "/ngo/:id/share" do
+  @ngo = Ngo.find(params[:id])
+  erb :"/ngo/share", locals: { errors: [] }
+end
